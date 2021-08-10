fbf0bfc6fe292467047f3d192bf3c8e49774918a
Jack Lim <jennystr233@gmail.com>
2021-08-10 12:54:47 +0900

    back up verticalEdit

---
+++ portfolio/src/pages/verticalEditSaveit.js
@@ -0,0 +1,23 @@
+/*
+<>
+      <NavButton />
+      <div className="divideArchipage">
+        <div className="initPosition">
+          <div className="archiLogo">
+            <div className="profileYhLink">
+              <Link to="/profile">
+                <span className="archiYhLinkSpan">YH</span>
+              </Link> 
+                <span className="archiDotLinkSpan">·</span> 
+              <Link to="/archi">
+                <span className="archiArchiLinkSpan">ARCHI</span> 
+              </Link>
+            </div>
+          </div>
+        </div>
+          <MainContents />
+          <Footer />
+        
+      </div>
+    </>
+*/
